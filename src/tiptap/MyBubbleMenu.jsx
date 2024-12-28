39aa17b24b55a13cf3d03993dadb476d3dbfb9fe
Marshall Powell <marshallpowell13@gmail.com>
2024-12-28 15:28:56 +0000

    added headings for bubble menu

--- src/tiptap/MyBubbleMenu.jsx
+++ src/tiptap/MyBubbleMenu.jsx
@@ -1,6 +1,5 @@
-import BulletList from "@tiptap/extension-bullet-list";
 import { BubbleMenu } from "@tiptap/react";
-import { Bold, Dot, Italic, Strikethrough } from "lucide-react";
+import { Bold, Dot, Heading1, Heading2, Heading3, Italic, ListOrdered, Strikethrough } from "lucide-react";
 import PropTypes from "prop-types";
 
 export default function MyBubbleMenu({ editor }) {
@@ -37,6 +36,30 @@ export default function MyBubbleMenu({ editor }) {
           className={`px-2 py-1 rounded ${editor.isActive('bulletList') ? 'bg-blue-500 text-white' : 'bg-white'}`}>
 
           <Dot />
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleOrderedList().run()}
+          className={`px-2 py-1 rounded ${editor.isActive('orderedList') ? 'bg-blue-500 text-white' : 'bg-white'}`}>
+
+          <ListOrdered />
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
+          className={`px-2 py-1 rounded ${editor.isActive('heading', { level: 1 }) ? 'bg-blue-500 text-white' : 'bg-white'}`}>
+
+          <Heading1 />
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
+          className={`px-2 py-1 rounded ${editor.isActive('heading', { level: 2 }) ? 'bg-blue-500 text-white' : 'bg-white'}`}>
+
+          <Heading2 />
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
+          className={`px-2 py-1 rounded ${editor.isActive('heading', { level: 3 }) ? 'bg-blue-500 text-white' : 'bg-white'}`}>
+
+          <Heading3 />
         </button>
       </BubbleMenu>
     </>
